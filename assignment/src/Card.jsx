6306abd29682af5fe1823f08a368e73f26339313
arcanevrse <sgtjulietnaboya54321@gmail.com>
2026-05-29 10:26:31 +0000

    secondcommit

---
+++ assignment/src/Card.jsx
@@ -0,0 +1,33 @@
+import "./index.css";
+
+function ProfileCard({ avatar, name, role, skill = ["none"] }) {
+  return (
+    <div className="flex flex-col items-center justify-center m-4">
+      <h1>{avatar}</h1>
+      <h3>{name}</h3>
+      <p>{role}</p>
+      <div>
+        <p>
+          Skill:
+          {skill.map((s) => (
+            <span key={s}>{s}</span>
+          ))}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export function Cards() {
+  return (
+    <>
+      <ProfileCard
+        avatar="🙍‍♂️"
+        name="DakOS"
+        role="Dev/Designer"
+        skill={["react"]}
+      />
+      <ProfileCard avatar="🙍‍♂️" name="Emma" role="Trader" />
+    </>
+  );
+}
